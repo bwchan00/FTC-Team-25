a6f56940381e9ccefd2ead04cdee4effc52af8c4
Breanna Chan <breannachan@rocknrollrobots25.com>
2017-11-22 15:59:23 -0800

    Added Disabled

---
+++ 2017/test/BreannaConfiguration.java
@@ -0,0 +1,22 @@
+package test;
+
+/**
+ * FTC Team 25: Created by Breanna Chan on 10/28/2017.
+ */
+
+public class BreannaConfiguration
+{
+    public final static int TICKS_PER_INCH = 68;
+    public final static int TICKS_PER_DEGREE = 29;
+    public final static double STRAIGHT_SPEED = 0.1;
+    public final static double TURN_SPEED = 0.5;
+
+    public final static double ODS_MIN = 0;
+    public final static double ODS_MAX = 0.8;
+
+    public final static double LEFT_DEPLOY_POS = 1;
+    public final static double RIGHT_DEPLOY_POS = 0;
+    public final static double RIGHT_STOW_POS = 1;
+    public final static double LEFT_STOW_POS = 0;
+
+}
