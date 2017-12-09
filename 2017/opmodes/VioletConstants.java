46d84e6523f7f4bfd3d4d5ca5c01921975ee22b0
Breanna Chan <breannachan@rocknrollrobots25.com>
2017-12-08 22:05:11 -0800

    Modified controls so bumpers to use claw correspond to top and bottom servo pairs. Added initial positions for claw servos.

--- 2017/opmodes/VioletConstants.java
+++ 2017/opmodes/VioletConstants.java
@@ -19,15 +19,19 @@ public class VioletConstants
 
     private static double SERVO_DOMAIN = 256.0;
 
-    public static double S1_OPEN      = 10  / SERVO_DOMAIN;
-    public static double S1_CLOSED    = 150  / SERVO_DOMAIN;
-    public static double S2_OPEN      = 255 / SERVO_DOMAIN;
-    public static double S2_CLOSED    = 120 / SERVO_DOMAIN;
-    public static double S3_OPEN      = 224  / SERVO_DOMAIN;
-    public static double S3_CLOSED    = 85  / SERVO_DOMAIN;
-    public static double S4_OPEN      = 10 / SERVO_DOMAIN;
-    public static double S4_CLOSED    = 140  / SERVO_DOMAIN;
     public static double JEWEL_INIT   = 145 / SERVO_DOMAIN;
+    public static double S1_INIT      = 10  / SERVO_DOMAIN;
+    public static double S2_INIT      = 255 / SERVO_DOMAIN;
+    public static double S3_INIT      = 224 / SERVO_DOMAIN;
+    public static double S4_INIT      = 10  / SERVO_DOMAIN;
+    public static double S1_OPEN      = 70  / SERVO_DOMAIN;
+    public static double S1_CLOSED    = 150 / SERVO_DOMAIN;
+    public static double S2_OPEN      = 188 / SERVO_DOMAIN;
+    public static double S2_CLOSED    = 120 / SERVO_DOMAIN;
+    public static double S3_OPEN      = 155 / SERVO_DOMAIN;
+    public static double S3_CLOSED    = 85  / SERVO_DOMAIN;
+    public static double S4_OPEN      = 75  / SERVO_DOMAIN;
+    public static double S4_CLOSED    = 140 / SERVO_DOMAIN;
     // Hesitant initial values. Will have to calibrate servo later.
     public static double RELIC_OPEN   = 100 / SERVO_DOMAIN;
     public static double RELIC_CLOSED = 170 / SERVO_DOMAIN;
